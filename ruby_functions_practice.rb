60427ab56ae67863bb16d85d8e54ef1a802fca36
Jack Morris <morriseo94@gmail.com>
2020-05-12 15:41:52 +0100

    Down to 8 errors

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,40 @@
+# TODO: write implementation here
+def return_five()
+    return 5
+end
+
+def return_ten()
+    return 10
+end
+
+def add(first_number, second_number)
+    return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+    return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+    return first_number * second_number
+end
+
+def divide(first_number, second_number)
+    return first_number / second_number
+end
+
+def length_of_string(string)
+    return "#{string}".length(string)
+end
+
+def join_string(string1, string2)
+    return "#{string1} + #{string2}"
+end
+
+def add_string_as_number(string1, string2)
+    return "#{string1}".to_i() + "#{string2}".to_i()
+end
+
+def number_to_full_name()
+    case month
+    

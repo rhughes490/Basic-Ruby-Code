73b088e95c6ee24c08d2665c8b10cde4ec62a50c
Jack Morris <morriseo94@gmail.com>
2020-05-12 16:34:11 +0100

    Just short month left

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -24,17 +24,37 @@ def divide(first_number, second_number)
 end
 
 def length_of_string(string)
-    return "#{string}".length(string)
+    return "#{string}".length()
 end
 
 def join_string(string1, string2)
-    return "#{string1} + #{string2}"
+    return "#{string1}" + "#{string2}"
 end
 
 def add_string_as_number(string1, string2)
     return "#{string1}".to_i() + "#{string2}".to_i()
 end
 
-def number_to_full_name()
-    case month
+def number_to_full_month_name(month_number)
+    case month_number
+    when 1
+        return "January"
+    when 3
+        return "March"
+    when 9
+        return "September"  
+   
+    end
     
+def number_to_short_month_name(month_number)
+    case month_number
+    when "1"
+        return "Jan"
+    when "4"
+        return "Apr"
+    when "10"
+        return "Oct"  
+   
+    end
+end
+end
